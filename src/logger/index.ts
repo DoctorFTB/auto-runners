455b26e19189755fa148359931a98bd36b1acdc4
FTB_lag <tabolskyy.git@gmail.com>
2024-07-19 00:04:28 +0300

    chore: some refactor logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -9,6 +9,13 @@ enum ELogLevel {
   ERROR,
 }
 
+const LOG_LEVEL_TO_EMOJI: Record<ELogLevel, string> = {
+  [ELogLevel.DEBUG]: '🤫',
+  [ELogLevel.INFO]: '✅',
+  [ELogLevel.WARN]: '❓',
+  [ELogLevel.ERROR]: '🆘',
+};
+
 const timeFormat = new Intl.DateTimeFormat(undefined, {
   year: 'numeric',
   hour: 'numeric',
@@ -18,68 +25,56 @@ const timeFormat = new Intl.DateTimeFormat(undefined, {
   month: '2-digit',
 });
 
-async function sendTelegramLog(level: string, text: string) {
-  const { chatId, token } = getConfig().telegram || {};
-
-  if (chatId && token) {
-    const data = {
-      chat_id: chatId,
-      text: level + ': ' + text,
-      disable_notification: true,
-    };
-    await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, data, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-}
-
 const {
   logger: { level: innerLevel, forceConsoleLogs },
 } = getConfig();
-const logLevel = ELogLevel[innerLevel];
 
-function isValidLevel(level: ELogLevel) {
-  return level >= logLevel;
-}
-
-// It's trash, I know... Maybe later I'll do better :) Also, PRs are welcome
 export class Logger {
-  public static info(text: string) {
-    const log = `[${timeFormat.format()}] ${text}`;
-    const valid = isValidLevel(ELogLevel.INFO);
+  private static logLevel = ELogLevel[innerLevel];
+
+  private static isValidLevel(level: ELogLevel) {
+    return level >= Logger.logLevel;
+  }
+
+  private static async sendTelegramLog(text: string, level: ELogLevel) {
+    const { chatId, token } = getConfig().telegram || {};
+
+    if (chatId && token) {
+      const data = {
+        chat_id: chatId,
+        text: LOG_LEVEL_TO_EMOJI[level] + ': ' + text,
+        disable_notification: true,
+      };
+
+      return await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, data, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  }
+
+  private static handleLog(rawText: string, level: ELogLevel) {
+    const text = `[${timeFormat.format()}] ${rawText}`;
+    const valid = Logger.isValidLevel(level);
 
     if (valid || forceConsoleLogs) {
-      console.log(log);
+      const method = ELogLevel[level].toLowerCase() as 'debug' | 'info' | 'warn' | 'error';
+      console[method](text);
     }
 
     if (valid) {
-      sendTelegramLog('✅', log);
+      return Logger.sendTelegramLog(text, level);
     }
+  }
+
+  public static info(text: string) {
+    return this.handleLog(text, ELogLevel.INFO);
   }
 
   public static warn(text: string) {
-    const log = `[${timeFormat.format()}] ${text}`;
-    const valid = isValidLevel(ELogLevel.WARN);
-
-    if (valid || forceConsoleLogs) {
-      console.warn(log);
-    }
-
-    if (valid) {
-      sendTelegramLog('❓', log);
-    }
+    return this.handleLog(text, ELogLevel.WARN);
   }
 
   public static error(text: string, additional: string) {
-    const log = `[${timeFormat.format()}] ${text}\n${additional}`;
-    const valid = isValidLevel(ELogLevel.ERROR);
-
-    if (valid || forceConsoleLogs) {
-      console.error(log);
-    }
-
-    if (valid) {
-      sendTelegramLog('🆘', log);
-    }
+    return this.handleLog(`${text}\n${additional}`, ELogLevel.ERROR);
   }
 }
